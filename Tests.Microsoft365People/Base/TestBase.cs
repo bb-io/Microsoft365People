68edbd3dfdf0343ebbe568b06c592f8c92b335c1
Anton Fesenko <tohafesenkolovep@gmail.com>
2026-03-19 15:02:48 +0200

    add tests

---
+++ Tests.Microsoft365People/Base/TestBase.cs
@@ -0,0 +1,28 @@
+﻿using Blackbird.Applications.Sdk.Common.Authentication;
+using Blackbird.Applications.Sdk.Common.Invocation;
+using Microsoft.Extensions.Configuration;
+
+namespace Tests.Microsoft365People.Base;
+
+public class TestBase
+{
+    public IEnumerable<AuthenticationCredentialsProvider> Creds { get; set; }
+
+    public InvocationContext InvocationContext { get; set; }
+
+    public FileManager FileManager { get; set; }
+
+    public TestBase()
+    {
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        Creds = config.GetSection("ConnectionDefinition").GetChildren().Select(x => new AuthenticationCredentialsProvider(x.Key, x.Value)).ToList();
+        var folderLocation = config.GetSection("TestFolder").Value;
+
+        InvocationContext = new InvocationContext
+        {
+            AuthenticationCredentialsProviders = Creds,
+        };
+
+        FileManager = new FileManager(folderLocation);
+    }
+}
